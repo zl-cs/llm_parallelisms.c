82ad6f2daf50a185632d8bbe59d2df1436eb9943
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2024-03-17 18:03:07 -0700

    Add histogram method.
    Also some temporary sdl stuff that I want to use to plot histograms. But it does nothing right now.

--- program.c
+++ program.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <SDL2/SDL.h>
+
+//Screen dimension constants.
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 double sqr(double x) {
 	return x * x;
@@ -31,10 +36,28 @@ double normal() {
 }
 
 
+int* histogram(double data[], int n_data, double low, double high, int n_bins) {
+	int* bins = calloc(n_bins, sizeof(int));
+
+	// TODO(eugenhotaj): We can speed this up by using binary search.
+	double step_size = (high - low) / n_bins;
+	for (int i = 0; i < n_data; i++) {
+		for (int j = 0; j < n_bins; j++) {
+			double boundary = low + (j+1) * step_size;
+			if (data[i] < boundary || j == n_bins - 1) {
+				bins[j] += 1;
+				break;
+			}
+		}
+	}
+	return bins;
+}
+
+
 int main(void) {
 	srand(time(NULL));
 
-	int n = 100000;
+	int n = 10000, n_bins = 10;
 
 	// Estimate the mean and variance.
 	double sum = 0.0, ssum = 0.0;
@@ -45,8 +68,61 @@ int main(void) {
 	}	
 	double mu = sum / n;
 	double sig = (ssum / n) - sqr(mu);
-
 	printf("mu: %f, sig: %f\n", mu, sig);
+
+
+	// Createa histogram of samples.
+	double samples[n];
+	for (int i = 0; i < n; i++) {
+		samples[i] = normal();
+	}
+	int* bins = histogram(samples, n, -3.0, 3.0, n_bins);
+
+	for (int i = 0; i < n_bins; i++) {
+		printf("%d ", bins[i]);
+	}
+	printf("\n");
+
+    return 0;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+		"SDL Tutorial", 
+		SDL_WINDOWPOS_UNDEFINED, 
+		SDL_WINDOWPOS_UNDEFINED, 
+		SCREEN_WIDTH, 
+		SCREEN_HEIGHT, 
+		SDL_WINDOW_SHOWN
+	);
+    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+
+
+	// Clear the screen.
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+	// Draw a rectangle.
+	SDL_Rect rect = {.x = 50, .y = 50, .w = 100, .h = 100};
+	SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00));
+
+	SDL_UpdateWindowSurface(window);
+
+	//Hack to get window to stay up.
+	SDL_Event e; 
+	int quit = 0; 
+	while(quit == 0) { 
+		while(SDL_PollEvent(&e)){ 
+			if(e.type == SDL_QUIT) {
+				quit = 1; 
+			} 
+		}
+	}
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 	return 0;
 }
 
