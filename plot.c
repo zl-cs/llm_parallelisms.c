d03348969ab6ddd3da536a6a151a24e1843b9311
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2024-08-25 15:13:28 -0700

    Split out plotting code to utils.

---
+++ plot.c
@@ -0,0 +1,135 @@
+#include <SDL2/SDL.h>
+
+//Screen dimension constants.
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+const int MAIN_RGBA[] = {0, 120, 255, 255};
+const int ACCENT_RGBA[] = {0, 60, 120, 255};
+
+
+double _max(double values[], int n_values) {
+	double max_ = values[0];
+	for (int i = 1; i < n_values; i++) {
+		if (values[i] > max_) {
+			max_ = values[i];
+		}
+	}
+	return max_;
+}
+
+double _min(double values[], int n_values) {
+	double min_ = values[0];
+	for (int i = 1; i < n_values; i++) {
+		if (values[i] < min_) {
+			min_= values[i];
+		}
+	}
+	return min_;
+}
+
+// TODO(eugenhotaj): We can speed this up by using binary search.
+double* _histogram(double data[], int n_data, int n_bins) {
+	double* bins = calloc(n_bins, sizeof(double));
+	double x_min = _min(data, n_data);
+	double x_max = _max(data, n_data);
+	double step_size = (x_max - x_min) / n_bins;
+	for (int i = 0; i < n_data; i++) {
+		for (int j = 0; j < n_bins; j++) {
+			double boundary = x_min + (j+1) * step_size;
+			if (data[i] < boundary || j == n_bins - 1) {
+				bins[j] += 1.0 / n_data;
+				break;
+			}
+		}
+	}
+	return bins;
+}
+
+
+void draw_histogram(SDL_Renderer *renderer, double data[], int n_data, int n_bins) { 
+	double* bins = _histogram(data, n_data, n_bins);
+	double y_max = _max(bins, n_bins);
+
+    double width = (double) SCREEN_WIDTH / n_bins;
+    for (int i = 0; i < n_bins; i++) {
+    	// Draw bar.
+        int height = bins[i] / y_max * SCREEN_HEIGHT;
+        SDL_Rect rect = {
+            .x = width * i,
+            .y = SCREEN_HEIGHT - height,
+            .w = width - 1,  // Leave 1 pixel gap between bars
+            .h = height
+        };
+        SDL_SetRenderDrawColor(renderer, MAIN_RGBA[0], MAIN_RGBA[1], MAIN_RGBA[2], MAIN_RGBA[3]);
+        SDL_RenderFillRect(renderer, &rect);
+        
+        // Draw outline.
+        SDL_SetRenderDrawColor(renderer, ACCENT_RGBA[0], ACCENT_RGBA[1], ACCENT_RGBA[2], ACCENT_RGBA[3]);
+        SDL_RenderDrawRect(renderer, &rect);
+    }
+}
+
+
+void draw_scatter_plot(
+	SDL_Renderer *renderer, 
+	double x[], 
+	double y[], 
+	int n, 
+	double x_min, 
+	double x_max, 
+	double y_min,
+	double y_max
+) {
+    SDL_SetRenderDrawColor(renderer, MAIN_RGBA[0], MAIN_RGBA[1], MAIN_RGBA[2], MAIN_RGBA[3]);
+    for (int i = 0; i < n; i++) {
+        int px = (x[i] - x_min) / (x_max - x_min) * SCREEN_WIDTH;
+        int py = SCREEN_HEIGHT - (y[i] - y_min) / (y_max - y_min) * SCREEN_HEIGHT;
+        
+        SDL_Rect point = {px - 2, py - 2, 4, 4};
+        SDL_RenderFillRect(renderer, &point);
+    }
+}
+
+
+void draw_points(
+	SDL_Renderer *renderer, 
+	double x[], 
+	double y[], 
+	int n, 
+	double x_min, 
+	double x_max, 
+	double y_min, 
+	double y_max
+) {
+    SDL_SetRenderDrawColor(renderer, MAIN_RGBA[0], MAIN_RGBA[1], MAIN_RGBA[2], MAIN_RGBA[3]);
+    for (int i = 0; i < n; i++) {
+        int px = (x[i] - x_min) / (x_max - x_min) * SCREEN_WIDTH;
+        int py = SCREEN_HEIGHT - (y[i] - y_min) / (y_max - y_min) * SCREEN_HEIGHT;
+        
+        SDL_Rect point = {px - 1, py - 1, 2, 2};
+        SDL_RenderFillRect(renderer, &point);
+    }
+
+}
+
+
+void clear_screen(SDL_Renderer *renderer) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+}
+
+
+void show_and_wait(SDL_Renderer *renderer) {
+	SDL_RenderPresent(renderer);
+    SDL_Event e;
+    int quit = 0;
+    while (quit == 0) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+    }
+	clear_screen(renderer);
+}
--- program.c
+++ program.c
@@ -2,39 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include <SDL2/SDL.h>
-
-//Screen dimension constants.
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-const int MAIN_RGBA[] = {0, 120, 255, 255};
-const int ACCENT_RGBA[] = {0, 60, 120, 255};
+#include "plot.c"
 
 
 double sqr(double x) {
 	return x * x;
 }
 
-double max(double values[], int n_values) {
-	double max_ = values[0];
-	for (int i = 1; i < n_values; i++) {
-		if (values[i] > max_) {
-			max_ = values[i];
-		}
-	}
-	return max_;
-}
-
-double min(double values[], int n_values) {
-	double min_ = values[0];
-	for (int i = 1; i < n_values; i++) {
-		if (values[i] < min_) {
-			min_= values[i];
-		}
-	}
-	return min_;
-}
 
 // Generates a sample from the uniform distribution. 
 // Assumes that srand() has already been called.
@@ -59,108 +33,6 @@ double normal() {
 	return v / u;
 }
 
-
-// TODO(eugenhotaj): We can speed this up by using binary search.
-double* histogram(double data[], int n_data, int n_bins) {
-	double* bins = calloc(n_bins, sizeof(double));
-	double x_min = min(data, n_data);
-	double x_max = max(data, n_data);
-	double step_size = (x_max - x_min) / n_bins;
-	for (int i = 0; i < n_data; i++) {
-		for (int j = 0; j < n_bins; j++) {
-			double boundary = x_min + (j+1) * step_size;
-			if (data[i] < boundary || j == n_bins - 1) {
-				bins[j] += 1.0 / n_data;
-				break;
-			}
-		}
-	}
-	return bins;
-}
-
-void draw_histogram(SDL_Renderer *renderer, double data[], int n_data, int n_bins) { 
-	double* bins = histogram(data, n_data, n_bins);
-	double y_max = max(bins, n_bins);
-
-    double width = (double) SCREEN_WIDTH / n_bins;
-    for (int i = 0; i < n_bins; i++) {
-    	// Draw bar.
-        int height = bins[i] / y_max * SCREEN_HEIGHT;
-        SDL_Rect rect = {
-            .x = width * i,
-            .y = SCREEN_HEIGHT - height,
-            .w = width - 1,  // Leave 1 pixel gap between bars
-            .h = height
-        };
-        SDL_SetRenderDrawColor(renderer, MAIN_RGBA[0], MAIN_RGBA[1], MAIN_RGBA[2], MAIN_RGBA[3]);
-        SDL_RenderFillRect(renderer, &rect);
-        
-        // Draw outline.
-        SDL_SetRenderDrawColor(renderer, ACCENT_RGBA[0], ACCENT_RGBA[1], ACCENT_RGBA[2], ACCENT_RGBA[3]);
-        SDL_RenderDrawRect(renderer, &rect);
-    }
-}
-
-
-void draw_scatter_plot(
-	SDL_Renderer *renderer, 
-	double x[], 
-	double y[], 
-	int n, 
-	double x_min, 
-	double x_max, 
-	double y_min,
-	double y_max
-) {
-    SDL_SetRenderDrawColor(renderer, MAIN_RGBA[0], MAIN_RGBA[1], MAIN_RGBA[2], MAIN_RGBA[3]);
-    for (int i = 0; i < n; i++) {
-        int px = (x[i] - x_min) / (x_max - x_min) * SCREEN_WIDTH;
-        int py = SCREEN_HEIGHT - (y[i] - y_min) / (y_max - y_min) * SCREEN_HEIGHT;
-        
-        SDL_Rect point = {px - 2, py - 2, 4, 4};
-        SDL_RenderFillRect(renderer, &point);
-    }
-}
-
-void draw_points(
-	SDL_Renderer *renderer, 
-	double x[], 
-	double y[], 
-	int n, 
-	double x_min, 
-	double x_max, 
-	double y_min, 
-	double y_max
-) {
-    SDL_SetRenderDrawColor(renderer, MAIN_RGBA[0], MAIN_RGBA[1], MAIN_RGBA[2], MAIN_RGBA[3]);
-    for (int i = 0; i < n; i++) {
-        int px = (x[i] - x_min) / (x_max - x_min) * SCREEN_WIDTH;
-        int py = SCREEN_HEIGHT - (y[i] - y_min) / (y_max - y_min) * SCREEN_HEIGHT;
-        
-        SDL_Rect point = {px - 1, py - 1, 2, 2};
-        SDL_RenderFillRect(renderer, &point);
-    }
-
-}
-
-void clear_screen(SDL_Renderer *renderer) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderClear(renderer);
-}
-
-void show_and_wait(SDL_Renderer *renderer) {
-	SDL_RenderPresent(renderer);
-    SDL_Event e;
-    int quit = 0;
-    while (quit == 0) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                quit = 1;
-            }
-        }
-    }
-	clear_screen(renderer);
-}
 
 void test_draw_gaussian() {
     SDL_Init(SDL_INIT_VIDEO);
@@ -187,6 +59,7 @@ void test_draw_gaussian() {
     SDL_DestroyWindow(window);
     SDL_Quit();
 } 
+
 
 void test_draw_gaussian_mixture() {
     SDL_Init(SDL_INIT_VIDEO);
@@ -218,6 +91,7 @@ void test_draw_gaussian_mixture() {
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
+
 
 void test_draw_linear_regression() {
     SDL_Init(SDL_INIT_VIDEO);
@@ -256,6 +130,7 @@ void test_draw_linear_regression() {
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
+
 
 int main(void) {
 	srand(time(NULL));
