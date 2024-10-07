c41af0c31b9fa167aad445659c0ef398aa42e2eb
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2024-10-06 17:06:41 -0700

    add tfidf

--- tf_idf.c
+++ tf_idf.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 unsigned long djb2(char* str) {
     unsigned long hash = 5381;
@@ -111,6 +112,11 @@ HashMapIterator* HashMapIterator_create(HashMap* map) {
     return iter;
 }
 
+void HashMapIterator_reset(HashMapIterator* iter, HashMap* map) {
+    iter->map = map;
+    iter->idx = -1;
+    iter->next = NULL;
+}
 
 HashNode* HashMapIterator_next(HashMapIterator* iter) {
     // First try to get the next item in the hash bucket.
@@ -193,44 +199,133 @@ int main(void) {
     // Create vocabulary.
     HashMap* vocabulary = HashMap_create(32768);
     int buffer_size = 1024;
+    int n_chunks = 0;
     char buffer[buffer_size];
     while (fgets(buffer, buffer_size, file)) {
+        // Remove blank lines.
+        if (buffer[0] == '\n' || buffer[0] == '\r') {
+            continue;
+        }
+
+        n_chunks += 1;
         char* token; 
-        token = strtok(buffer, " ");
+        token = strtok(buffer, " \n\r");
         while (token != NULL) {
             if (HashMap_get_int(vocabulary, token) < 0) {
                 HashMap_insert(vocabulary, token, vocabulary->size, -1.0);
             }
-            token = strtok(NULL, " ");
+            token = strtok(NULL, " \n\r");
         }
     }
-    printf("Vocabulary size: %d\n", vocabulary->size);
+    printf("Vocabulary: %d, chunks: %d\n", vocabulary->size, n_chunks);
 
 
+    // TODO(eugen): This part of the code duplicates the file reading above
+    // and has lead to some bugs when we change one but not the other. 
     // Chunk and embed text. 
     // For now, each line is a separate chunk.
     rewind(file);
-    int n_chunks = 40000;
     HashMap* vectors[n_chunks];
     int i = 0;
     while (fgets(buffer, buffer_size, file)) {
-        HashMap* vec = HashMap_create(32);
+        // Remove blank lines.
+        if (buffer[0] == '\n' || buffer[0] == '\r') {
+            continue;
+        }
 
+        HashMap* vec = HashMap_create(32);
         char* token; 
-        token = strtok(buffer, " ");
+        token = strtok(buffer, " \n\r");
         while (token) {
             int idx = HashMap_get_int(vocabulary, token);
             if (idx >= 0) {
                 float value = HashMap_get_float(vec, token);
                 HashMap_insert(vec, token, idx, 1.0 ? value < 0 : value + 1.0);
             }
-            token = strtok(NULL, " ");
+            token = strtok(NULL, " \n\r");
         }
 
         vectors[i] = vec;
         i += 1;
     }
     fclose(file);
+
+
+    // This iterator will be reused in all the code below. Use a dummy
+    // HashMap to initialize with for now. 
+    HashMapIterator* iter = HashMapIterator_create(vocabulary);  
+
+    // Compute term frequencies. 
+    for (int i = 0; i < n_chunks; i++) {
+        HashMapIterator_reset(iter, vectors[i]);
+        HashNode* node = HashMapIterator_next(iter);
+        float total = 0.0;
+        while (node) {
+            total += node->float_value;
+            node = HashMapIterator_next(iter);
+        }
+
+        HashMapIterator_reset(iter, vectors[i]);
+        node = HashMapIterator_next(iter);
+        while (node) {
+            node->float_value /= total;
+            node = HashMapIterator_next(iter);
+        }
+    }
+
+    // Compute inverse document frequencies.
+    HashMap* idf = HashMap_create(vocabulary->size);
+    for (int i = 0; i < n_chunks; i++) {
+        HashMapIterator_reset(iter, vectors[i]);
+        HashNode* node = HashMapIterator_next(iter);
+        while (node) {
+            int curr_df = HashMap_get_int(idf, node->key);
+            curr_df = 1 ? curr_df < 0 : curr_df + 1;
+            float curr_idf = log(n_chunks / curr_df);
+            HashMap_insert(idf, node->key, curr_df, curr_idf);
+            node = HashMapIterator_next(iter);
+        }
+    }
+
+    // Update vectors with tf-idf score.
+    for (int i = 0; i < n_chunks; i++) {
+        HashMapIterator_reset(iter, vectors[i]);
+        HashNode* node = HashMapIterator_next(iter);
+        while (node) {
+            float idf_score = HashMap_get_float(idf, node->key);
+            if (idf_score < 0.0) {
+                idf_score = log(n_chunks);
+            }
+            node->float_value = node->float_value * idf_score;
+            node = HashMapIterator_next(iter);
+        }
+    }
+
+    // Print top k;
+    Score scores[n_chunks];
+    int idx = 128, k = 10;
+    score_and_sort(vectors[idx], vectors, n_chunks, scores);
+
+    printf("Query vector (%d): ", idx);
+    HashMapIterator_reset(iter, vectors[idx]);
+    HashNode* node = HashMapIterator_next(iter);
+    while (node) {
+        printf("%s ", node->key);
+        node = HashMapIterator_next(iter);
+    }   
+    printf("\n\n");
+
+    printf("Top matches:\n");
+    for (int i = 0; i < k; i++) {
+        printf("(%5d, %5.2f) -> ", scores[i].idx, scores[i].score);
+        HashMapIterator_reset(iter, vectors[scores[i].idx]);
+        HashNode* node = HashMapIterator_next(iter);
+        while (node) {
+            printf("%s ", node->key);
+            node = HashMapIterator_next(iter);
+        }
+        printf("\n");
+    }
 
 	return 0;
 }
