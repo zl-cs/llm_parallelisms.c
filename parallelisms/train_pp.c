352e103a755149c3076d0c78a1c31b17c1e10f6a
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2024-10-24 16:02:18 -0700

    tp train + valid

--- parallelisms/train_pp.c
+++ parallelisms/train_pp.c
@@ -27,24 +27,22 @@ Model* Model_create_rank_stage(
     Model* self = Model_create(batch_size, seq_len, vocab_size, emb_size, hidden_size);
 
     if (rank == 0) {
-        Linear_destroy(self->fc_1);
-        Linear_destroy(self->fc_2);
-        self->wte_out_flat = NULL;
-        Activation_destory(self->fc_1_out);
-        Activation_destory(self->relu_out);
-        Activation_destory(self->fc_2_out);
-        Activation_destory(self->softmax_out);
+        Linear_destroy(self->fc_1); self->fc_1 = NULL;
+        Linear_destroy(self->fc_2); self->fc_2 = NULL;
+        Activation_destory(self->fc_1_out); self->fc_1_out = NULL;
+        Activation_destory(self->relu_out); self->relu_out = NULL;
+        Activation_destory(self->fc_2_out); self->fc_2_out = NULL;
+        Activation_destory(self->softmax_out); self->softmax_out = NULL;
     } else if (rank == 1) {
-        Embedding_destory(self->wte);
-        Linear_destroy(self->fc_2);
-        Activation_destory(self->fc_2_out);
-        Activation_destory(self->softmax_out);
+        Embedding_destory(self->wte); self->wte = NULL;
+        Linear_destroy(self->fc_2); self->fc_2 = NULL;
+        Activation_destory(self->fc_2_out); self->fc_2_out = NULL;
+        Activation_destory(self->softmax_out); self->softmax_out = NULL;
     } else if (rank == 2) {
-        Embedding_destory(self->wte);
-        Linear_destroy(self->fc_1);
-        Activation_destory(self->wte_out);
-        self->wte_out_flat = NULL;
-        Activation_destory(self->fc_1_out);
+        Embedding_destory(self->wte); self->wte = NULL;
+        Linear_destroy(self->fc_1); self->fc_1 = NULL;
+        Activation_destory(self->wte_out); self->wte_out = NULL; self->wte_out_flat = NULL;
+        Activation_destory(self->fc_1_out); self->fc_1_out = NULL;
     } else {
         printf("Unknown rank: %d\n", rank);
         MPI_Finalize();
@@ -77,8 +75,64 @@ float Model_forward_pp(Model* self, int* Xs, int* Ys, int rank) {
     }
     // We don't technically need to broadcast here, but it's nicer if all the ranks have the
     // same loss value at the end.
-    MPI_Bcast(&loss, /* count */ 1, MPI_FLOAT, 3, MPI_COMM_WORLD);
+    MPI_Bcast(&loss, /* count */ 1, MPI_FLOAT, /* root */ 2, MPI_COMM_WORLD);
     return loss;
+}
+
+
+void Model_backward_pp(Model* self, int* Xs, int* Ys, int rank) {
+    if (rank == 2) {
+        // Zero grad.
+        memset(self->fc_2->d_weight, 0, sizeof(float) * Linear_weight_numel(self->fc_2));
+        memset(self->fc_2->d_bias, 0, sizeof(float) * self->fc_2->out_features);
+        memset(self->relu_out->d_value, 0, sizeof(float) * Activation_numel(self->relu_out));
+        memset(self->fc_2_out->d_value, 0, sizeof(float) * Activation_numel(self->fc_2_out));
+        memset(self->softmax_out->d_value, 0, sizeof(float) * Activation_numel(self->softmax_out));
+        // Backward.
+        cross_entropy_softmax_backward(self->fc_2_out, self->softmax_out, Ys);
+        Linear_backward(self->fc_2, self->relu_out, self->fc_2_out);
+        send(self->relu_out->d_value, Activation_numel(self->relu_out), /* to_rank */ 1);
+    } else if (rank == 1) {
+        // Zero grad.
+        memset(self->fc_1->d_weight, 0, sizeof(float) * Linear_weight_numel(self->fc_1));
+        memset(self->fc_1->d_bias, 0, sizeof(float) * self->fc_1->out_features);
+        memset(self->wte_out_flat->d_value, 0, sizeof(float) * Activation_numel(self->wte_out));
+        memset(self->fc_1_out->d_value, 0, sizeof(float) * Activation_numel(self->fc_1_out));
+        memset(self->relu_out->d_value, 0, sizeof(float) * Activation_numel(self->relu_out));
+        // Backward.
+        recv(self->relu_out->d_value, Activation_numel(self->relu_out), /* from_rank */ 2);
+        relu_backward(self->fc_1_out, self->relu_out);
+        Linear_backward(self->fc_1, self->wte_out_flat, self->fc_1_out);
+        send(self->wte_out_flat->d_value, Activation_numel(self->wte_out_flat), /* to_rank */ 0);
+    } else if (rank == 0) {
+        // Zero grad.
+        memset(self->wte->d_embedding, 0, sizeof(float) * Embedding_numel(self->wte));
+        memset(self->wte_out->d_value, 0, sizeof(float) * Activation_numel(self->wte_out));
+        // Backward.
+        recv(self->wte_out->d_value, Activation_numel(self->wte_out), /* from_rank */ 1);
+        Embedding_backward(self->wte, Xs, self->wte_out);
+    } else {
+        printf("Unknown rank: %d\n", rank);
+        MPI_Finalize();
+        exit(1);
+    }
+}
+
+
+void Model_step_pp(Model* self, float lr, int rank) {
+    if (rank == 0) {
+        sgd_step(self->wte->embedding, self->wte->d_embedding, Embedding_numel(self->wte), lr);
+    } else if (rank == 1) {
+        sgd_step(self->fc_1->weight, self->fc_1->d_weight, Linear_weight_numel(self->fc_1), lr);
+        sgd_step(self->fc_1->bias, self->fc_1->d_bias, self->fc_1->out_features, lr);
+    } else if (rank == 2) {
+        sgd_step(self->fc_2->weight, self->fc_2->d_weight, Linear_weight_numel(self->fc_2), lr);
+        sgd_step(self->fc_2->bias, self->fc_2->d_bias, self->fc_2->out_features, lr);
+    } else {
+        printf("Unknown rank: %d\n", rank);
+        MPI_Finalize();
+        exit(1);
+    }
 }
 
 
@@ -111,9 +165,26 @@ int main(int argc, char** argv) {
     // Create model.
     Model* model = Model_create_rank_stage(batch_size, seq_len, vocab_size, emb_size, hidden_size, rank);
 
-    Dataset_get_batch(&train_split, Xs, Ys, batch_size);
-    float loss = Model_forward_pp(model, Xs, Ys, world_size);
-    rank0_printf(rank, "step: %d, loss %f\n", 0, loss);
+    // Train.
+    float lr = 0.1;
+    int steps = 25000;
+    for (int step = 0; step < steps; step++) {
+        Dataset_get_batch(&train_split, Xs, Ys, batch_size);
+        float loss = Model_forward_pp(model, Xs, Ys, rank);
+        rank0_printf(rank, "step: %d, loss %f\n", step, loss);
+        Model_backward_pp(model, Xs, Ys, rank);
+        Model_step_pp(model, lr, rank);
+    }
+
+    // Validate.
+    float loss = 0.0f;
+    int n_valid_batches = 100;
+    for (int i = 0; i < n_valid_batches; i ++) {
+        Dataset_get_batch(&test_split, Xs, Ys, batch_size);
+        loss += Model_forward_pp(model, Xs, Ys, rank);
+    }
+    rank0_printf(rank, "Final validation loss: %f\n", loss / n_valid_batches);
+
 
     MPI_Finalize();
     return 0;
